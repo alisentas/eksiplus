5af84cf344af56d96cd806c2a26e5f1cdfce42e2
Ali Sentas <alisentas96@gmail.com>
2015-08-22 17:45:22 +0300

    search text counter added

--- data/utils.js
+++ data/utils.js
@@ -1,3 +1,4 @@
+// ability to search authors other entries in the title
 $("#entry-list .info").each(function(){
     var url = $("#title a").attr("href");
     var searchAuthor = document.createElement("a");
@@ -9,4 +10,15 @@ $("#entry-list .info").each(function(){
     searchAuthorLi.appendChild(searchAuthor);
 
     $(this).find(".other ul").append(searchAuthorLi);
+})
+
+
+// search text counter
+var counter = document.createElement("span");
+counter.textContent = 0;
+counter.className = "eksiplus-search-counter";
+$("#search-form").prepend(counter);
+
+$("#search-textbox").keyup(function(){
+    $(".eksiplus-search-counter").text($(this).val().length);
 })
